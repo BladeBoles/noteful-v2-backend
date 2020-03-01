da72ca55614fa1c7c1d5adf331f2f549fd132a64
Blade Boles <bladerboles@gmail.com>
2020-02-29 19:55:31 -0700

    For deployment

--- src/app.js
+++ src/app.js
@@ -19,6 +19,16 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(function(req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
+
+});
+
 app.use('/api/notes', noteRouter);
 app.use('/api/folders', folderRouter);
 
